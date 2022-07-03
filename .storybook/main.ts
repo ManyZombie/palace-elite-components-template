5f9ba85402d904aab65dccff1ecc1c3a4d778aab
Emmanuel Yañez Martinez <manyzombie@hotmail.com>
2022-07-03 07:02:08 -0500

    Update storybook config files to work with typescript

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,28 +1,20 @@
+// import type { StorybookViteConfig } from 'storybook-builder-vite-vue2';
 const { loadConfigFromFile, mergeConfig } = require("vite");
 const path = require("path");
 
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/vue",
-  "core": {
-    "builder": "storybook-builder-vite-vue2"
-  },
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
+  framework: "@storybook/vue",
+  core: {
+    builder: "storybook-builder-vite-vue2"
   },
   async viteFinal(previousConfig) {
     const { config } = await loadConfigFromFile(
