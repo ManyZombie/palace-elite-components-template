caafb96addf37bcd5aab44b2cbd75645a9a3364a
Emmanuel Yañez Martinez <manyzombie@hotmail.com>
2022-07-04 15:49:29 -0500

    Added eslint with airbnb config, vue recommended, prettier and typscript config

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+    'vue/setup-compiler-macros': true,
+  },
+  extends: [
+    // 'eslint:recommended',
+    'airbnb-base',
+    '@vue/prettier',
+    '@vue/typescript/recommended',
+    'plugin:vue/recommended',
+    'plugin:storybook/recommended',
+    'plugin:markdown/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'vue/max-attributes-per-line': 'off',
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { resolve } from "path";
 // New plugin for vue 2 compatibility when storybook-builder-vite-vue2 
 // updates should remove vite-plugin-vue2 and vue-template-compiler
 import vue from "@vitejs/plugin-vue2";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
