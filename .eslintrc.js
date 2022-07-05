57904917d06bac079b19f46d6c8df8e8d8498ce7
Emmanuel Yañez Martinez <manyzombie@hotmail.com>
2022-07-05 15:59:14 -0500

    Added eslint with airbnb config  and pettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,20 +7,36 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
-    '@vue/prettier',
-    '@vue/typescript/recommended',
+    'airbnb-typescript/base',
     'plugin:vue/recommended',
-    'plugin:storybook/recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier-vue/recommended',
     'plugin:markdown/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
+    'prettier',
   ],
   parserOptions: {
     ecmaVersion: 2022,
+    project: './tsconfig.json',
   },
   rules: {
     'no-console': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'vue/max-attributes-per-line': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
+  overrides: [
+    {
+      files: ['*.stories.@(ts|tsx|js|jsx|mjs|cjs|mdx)'],
+      parserOptions: {
+        ecmaVersion: 2022,
+        project: './.storybook/tsconfig.json',
+      },
+      extends: ['plugin:storybook/recommended'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
 };
